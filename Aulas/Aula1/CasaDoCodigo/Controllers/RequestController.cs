dd68d933e4f990db4b0805189627723f6514ee72
HunterAdv <gustavopaulody@gmail.com>
2022-06-02 14:58:10 -0300

    Adicionando páginas.

---
+++ Aulas/Aula1/CasaDoCodigo/Controllers/RequestController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CasaDoCodigo.Controllers
+{
+    public class RequestController : Controller
+    {
+        public IActionResult Register()
+        {
+            return View();
+        }
+        public IActionResult Cart()
+        {
+            return View();
+        }
+        public IActionResult Carousel()
+        {
+            return View();
+        }
+        public IActionResult Summary()
+        {
+            return View();
+        }
+    }
+}
