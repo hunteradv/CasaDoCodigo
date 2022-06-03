9dc1801115f55802823dbea6f29601f644433df7
HunterAdv <gustavopaulody@gmail.com>
2022-06-03 11:45:00 -0300

    Incluindo método InitializeBD para garantir que será criado um bd ao iniciar a aplicação

---
+++ Aulas/Aula1/CasaDoCodigo/DataService.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.IO;
+
+namespace CasaDoCodigo
+{
+    public class DataService : IDataService
+    {
+        private readonly ApplicationContext context;
+        public DataService(ApplicationContext context)
+        {
+            this.context = context;
+        }
+
+        public void InitializeDB()
+        {
+            context.Database.Migrate();
+
+            var json = File.ReadAllText("books.json");
+            var books = JsonConvert.DeserializeObject<List<Book>>(json);
+        }
+    }
+
+    public class Book
+    {
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+    }
+}
--- Aulas/Aula1/CasaDoCodigo/Startup.cs
+++ Aulas/Aula1/CasaDoCodigo/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json;
 
 namespace CasaDoCodigo
 {
@@ -29,6 +30,8 @@ namespace CasaDoCodigo
             services.AddDbContext<ApplicationContext>(options => 
                 options.UseSqlServer(connectionString)
             );
+
+            services.AddTransient<IDataService, DataService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -54,7 +57,7 @@ namespace CasaDoCodigo
                     template: "{controller=Request}/{action=Carousel}/{id?}");
             });
 
-            serviceProvider.GetService<ApplicationContext>().Database.Migrate();
+            serviceProvider.GetService<IDataService>().InitializeDB();
         }
     }
 }
