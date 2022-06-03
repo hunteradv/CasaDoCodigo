26e6e42a0eac8cde281ec64e5ca411e0b66e112a
HunterAdv <gustavopaulody@gmail.com>
2022-06-03 09:09:53 -0300

    Terminando de configurar contexto e adicionando método migrate() na startup

--- Aulas/Aula1/CasaDoCodigo/ApplicationContext.cs
+++ Aulas/Aula1/CasaDoCodigo/ApplicationContext.cs
@@ -15,6 +15,21 @@ namespace CasaDoCodigo
             base.OnModelCreating(modelBuilder);
 
             modelBuilder.Entity<Product>().HasKey(p => p.Id);
+
+            
+            modelBuilder.Entity<Request>().HasKey(p => p.Id);
+            //tabela 1 - n (cada pedido tem muitos itens de pedido)
+            modelBuilder.Entity<Request>().HasMany(p => p.Items).WithOne(p => p.Request);
+            //tabela 1 - 1 (cada pedido tem um cadastro)
+            modelBuilder.Entity<Request>().HasOne(p => p.Register).WithOne(p => p.Request).IsRequired();
+
+            //tabela 1 - 1
+            modelBuilder.Entity<ItemRequest>().HasKey(p => p.Id);
+            modelBuilder.Entity<ItemRequest>().HasOne(p => p.Request);
+            modelBuilder.Entity<ItemRequest>().HasOne(p => p.Product);
+
+            modelBuilder.Entity<Register>().HasKey(p => p.Id);
+            modelBuilder.Entity<Register>().HasOne(p => p.Request);
         }
     }
 }
---
+++ Aulas/Aula1/CasaDoCodigo/Migrations/20220603112326_Model.cs
@@ -0,0 +1,109 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace CasaDoCodigo.Migrations
+{
+    public partial class Model : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Register",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Address = table.Column<string>(nullable: false),
+                    CEP = table.Column<string>(nullable: false),
+                    City = table.Column<string>(nullable: false),
+                    Complement = table.Column<string>(nullable: false),
+                    District = table.Column<string>(nullable: false),
+                    Email = table.Column<string>(nullable: false),
+                    Name = table.Column<string>(nullable: false),
+                    Telephone = table.Column<string>(nullable: false),
+                    UF = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Register", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Request",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    RegisterId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Request", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Request_Register_RegisterId",
+                        column: x => x.RegisterId,
+                        principalTable: "Register",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ItemRequest",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    ProductId = table.Column<int>(nullable: false),
+                    Quantity = table.Column<int>(nullable: false),
+                    RequestId = table.Column<int>(nullable: false),
+                    UnitPrice = table.Column<decimal>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ItemRequest", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ItemRequest_Product_ProductId",
+                        column: x => x.ProductId,
+                        principalTable: "Product",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ItemRequest_Request_RequestId",
+                        column: x => x.RequestId,
+                        principalTable: "Request",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ItemRequest_ProductId",
+                table: "ItemRequest",
+                column: "ProductId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ItemRequest_RequestId",
+                table: "ItemRequest",
+                column: "RequestId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Request_RegisterId",
+                table: "Request",
+                column: "RegisterId",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ItemRequest");
+
+            migrationBuilder.DropTable(
+                name: "Request");
+
+            migrationBuilder.DropTable(
+                name: "Register");
+        }
+    }
+}
