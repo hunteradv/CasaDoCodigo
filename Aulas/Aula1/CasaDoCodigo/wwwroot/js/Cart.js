6e73471c9c4075e31f87c85d91ce34458c02f4d1
HunterAdv <gustavopaulody@gmail.com>
2022-06-10 08:46:22 -0300

    incluindo metodo clickDecrement() no js

---
+++ Aulas/Aula1/CasaDoCodigo/wwwroot/js/Cart.js
@@ -0,0 +1,35 @@
+﻿class Cart {
+    clickIncrement(btn) {
+        let data = this.getData(btn);
+        data.Quantity++;
+        this.postQuantity(data);               
+    }
+
+    clickDecrement() {
+        let data = this.getData(btn);
+        data.Quantity--;
+        this.postQuantity(data);
+    }
+
+    getData(element) {
+        let itemLine = $(element).parents('[item-id]');
+        let itemId = $(itemLine).attr('item-id');
+        let newQuantity = $(itemLine).find('input').val();
+
+        return {
+            Id: itemId,
+            Quantity: newQuantity
+        };
+    }
+
+    postQuantity(data) {
+        $.ajax({
+            url: '/request/updatequantity',
+            type: 'POST',
+            contentType: 'application/json',
+            data: JSON.stringify(data)
+        });
+    }
+}
+
+var cart = new Cart();
