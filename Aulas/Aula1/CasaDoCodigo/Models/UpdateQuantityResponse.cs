43e14d6f76e1bff754ca8083b66d0b4208e68fbb
HunterAdv <gustavopaulody@gmail.com>
2022-06-10 12:09:37 -0300

    Adicionando classe UpdateQuantityResponse

---
+++ Aulas/Aula1/CasaDoCodigo/Models/UpdateQuantityResponse.cs
@@ -0,0 +1,16 @@
+﻿using CasaDoCodigo.Models.ViewModels;
+
+namespace CasaDoCodigo.Models
+{
+    public class UpdateQuantityResponse
+    {
+        public UpdateQuantityResponse(ItemRequest itemRequest, CartViewModel cartViewModel)
+        {
+            ItemRequest = itemRequest;
+            CartViewModel = cartViewModel;
+        }
+
+        public ItemRequest ItemRequest { get; }
+        public CartViewModel CartViewModel { get; }
+    }
+}
